cde19cea48b14de3b056c02ec87a677cc0b17e87
gis-11070578 <sophia.soni@student.manchester.ac.uk>
2026-01-24 14:53:41 +0000

    starting code - setup

--- dissertation-code.py
+++ dissertation-code.py
@@ -1,16 +1,26 @@
 """
-Understanding GIS: Assessment 2
-@author [INSERT STUDENT NUMBER HERE]
+Dissertation - [insert title here]
+@author [11070578]
 """
 from time import perf_counter
 
 # set start time
 start_time = perf_counter()	
 
-# --- NO CODE ABOVE HERE ---
+# --- NO CODE ABOVE HERE -------
+
+# All Imports -------------------------------
+
+from geopandas import read_file, gpd
+from rasterio import open as rio_open
+from rasterio.transform import rowcol
+from rasterio.plot import show as rio_show
+
+# All Functions ---------------------------------
+
+# Main Code -------------------------------------
 
 
-''' --- ALL CODE MUST BE INSIDE HERE --- '''
 
 
 # --- NO CODE BELOW HERE ---
