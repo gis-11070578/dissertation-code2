6d0870a6486f68c26768e66367ce4fea5fb398b0
gis-11070578 <sophia.soni@student.manchester.ac.uk>
2026-01-24 15:46:17 +0000

    trying to do pseudocode

--- dissertation-code.py
+++ dissertation-code.py
@@ -20,6 +20,34 @@ from rasterio.plot import show as rio_show
 
 # Main Code -------------------------------------
 
+# Load all spatial data ----------
+
+#load all contours - vector
+contours = read_file("../data/SlopeContour_polygon.shp")
+
+#load all landuse - vector
+Land_Use = read_file("../data/Land-Use-All")
+
+#NOT COMPLETED YET
+#load all flood zone 2 - raster 
+floodzone_2 = rio_open("../data/")
+
+#load all flood zone 3 - raster 
+floodzone_3 = rio_open("../data/")
+
+
+#SECTION 1 - Cutting each landuse polygon based on elevation cut off --------
+
+
+#SECTION 2 - Finding max circle in each polygon ------------------------
+
+
+#SECTION 3 - Creating weighted overlays (user defined) ------------------
+
+
+
+# Plotting all Maps ------------------------------
+
 
 
 
